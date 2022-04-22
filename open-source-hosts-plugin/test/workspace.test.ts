3455ae1e022687cff7331b3e1455bb8cb055f3f9
Hao Zhao <36955586+eherozhao@users.noreply.github.com>
2022-04-21 21:07:31 -0700

    change exec in workspace to execFile for security

--- open-source-hosts-plugin/test/workspace.test.ts
+++ open-source-hosts-plugin/test/workspace.test.ts
@@ -1,3 +1,13 @@
+import childProcess from 'child_process';
+import fs from 'fs-extra';
+import {
+  AssetsNotFoundError,
+  installDependencies,
+  prepareWorkspace,
+  validateAssetsPath,
+  WorkspaceNotPreparedError,
+} from '../src/workspace';
+
 jest.mock('fs-extra', () => ({
   ...jest.requireActual('fs-extra'),
   promises: {
@@ -5,12 +15,17 @@ jest.mock('fs-extra', () => ({
     mkdir: jest.fn().mockResolvedValue(null),
   },
   copy: jest.fn(),
-  existsSync: jest.fn()
+  existsSync: jest.fn(),
 }));
 
-import fs from 'fs-extra';
-import {prepareWorkspace, AssetsNotFoundError, validateAssetsPath, WorkspaceNotPreparedError} from '../src/workspace';
-
+jest.mock('child_process', () => ({
+  ...jest.requireActual('child_process'),
+  exec: jest.fn().mockImplementation(() => ({
+    stdout: 'fake stdout',
+    stderr: 'fake stderr',
+  })),
+}));
+jest.mock('util', () => ({promisify: (fn) => fn}));
 
 describe('workspace', () => {
   beforeEach(() => {
@@ -21,15 +36,21 @@ describe('workspace', () => {
     const mockCopy = jest.fn();
     jest.spyOn(fs, 'copy').mockImplementation(mockCopy);
     await prepareWorkspace('testPluginDir', 'testWorkSpaceDir');
+
     expect(fs.promises.copyFile).toHaveBeenCalledTimes(1);
     expect(fs.promises.mkdir).toHaveBeenCalledTimes(1);
   });
 
-  it('should throw custom WorkspaceNotPreparedError when there are errors with fs package', async() => {
-    jest.spyOn(fs.promises, 'copyFile').mockRejectedValue(new WorkspaceNotPreparedError("Mocked Error"));
+  it('should throw custom WorkspaceNotPreparedError when there are errors with fs package', async () => {
+    jest
+      .spyOn(fs.promises, 'copyFile')
+      .mockRejectedValue(new WorkspaceNotPreparedError('Mocked Error'));
     const mockCopy = jest.fn();
     jest.spyOn(fs, 'copy').mockImplementation(mockCopy);
-    await expect(prepareWorkspace('testPluginDir', 'testWorkSpaceDir')).rejects.toThrow(WorkspaceNotPreparedError);
+
+    await expect(
+      prepareWorkspace('testPluginDir', 'testWorkSpaceDir')
+    ).rejects.toThrow(WorkspaceNotPreparedError);
   });
 
   it('should return nothing when the assetDir exists', () => {
@@ -37,6 +58,7 @@ describe('workspace', () => {
       .spyOn(fs, 'existsSync')
       .mockReturnValueOnce(true);
     validateAssetsPath('testPluginDir');
+
     expect(spyExistsSync).toHaveBeenCalledTimes(1);
   });
 
@@ -45,11 +67,17 @@ describe('workspace', () => {
     function testValidateAsset() {
       validateAssetsPath('testPluginDir');
     }
+
     expect(testValidateAsset).toThrowError(
       'Sumerian Host assets could not be found at '
     );
+
     expect(testValidateAsset).toThrowError(AssetsNotFoundError);
   });
-});
 
-debugger;
+  it('should call exec once', async () => {
+    await installDependencies('testWorkSpaceDir', {testKey: ''});
+
+    expect(childProcess.exec).toHaveBeenCalledTimes(1);
+  });
+});
